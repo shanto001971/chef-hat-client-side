28f2c0dcad22dba6b65818ff7e8de7d6f7011b24
Shanto Islam <rajs45693@gmail.com>
2023-05-01 22:47:20 +0600

    Footer ok

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,8 +2,23 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            <h1>this is footer section</h1>
+        <div className='p-2 bg-slate-400'>
+            <div className="text-center mt-5">
+                <h1>
+                A chef is a professional <br /> cook and tradesman who is proficient in <br /> all aspects of food preparation, often focusing on <br /> a particular cuisine.etc?
+                </h1>
+            </div>
+            <div className=" mt-5 lg:flex justify-between">
+                <div className="">
+                <h1>Favreau wrote the script after <br /> directing several big-budget films, <br /> wanting to go "back to basics" and to create <br /> a film about cooking.[5] Food truck owner and <br /> chef Roy Choi served as a co-producer</h1>
+                </div>
+                <div className="mt-5">
+                    <h1>When Carl has the chance to serve <br /> prestigious food critic and blogger <br /> Ramsey Michel, Riva demands he prepare <br />  old favorites at the last minute; Carl concedes, <br /> leading to a scathing review.</h1>
+                </div>
+            </div>
+            <div className="border text-center mt-5 p-5">
+                <h1>Copy right by Make Chafe</h1>
+            </div>
         </div>
     );
 };
