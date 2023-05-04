a103e3b805189ada1196b7aae336a4c967b77e58
Shanto Islam <rajs45693@gmail.com>
2023-05-04 19:44:07 +0600

    bannar section Upgrade

--- src/components/Homes/Home.jsx
+++ src/components/Homes/Home.jsx
@@ -10,12 +10,12 @@ const Home = () => {
     const [loading, setLoading] = useState(true)
     const [chefData, setChefData] = useState([])
 
-    
+
     useEffect(() => {
         fetch('https://make-chef-sarver-site-shanto001971.vercel.app/data')
             .then(res => res.json())
             .then(data => setChefData(data))
-            setLoading(false)
+        setLoading(false)
     }, [])
 
     if (loading) {
@@ -23,12 +23,15 @@ const Home = () => {
     }
     return (
         <div className=''>
-            <div style={{ backgroundImage: `url("https://pikwizard.com/pw/small/ffc0672f15697ae9b994f7ee2b3ee9eb.jpg")` }} className="bg-cover h-96 bg-no-repeat text-center   md:filter-none">
-                <div className="">
-                    <input type="text" placeholder="Type here" className="input w-full max-w-xs my-48" />
-                    <button className=' p-4'><FaSearch className='w-10 text-white' /></button>
-                    
-                    
+            <div className=" mb-5  lg:flex gap-5">
+                <div className="h-2/6 ">
+                    <img className='rounded-md' src="https://media.defense.gov/2018/May/24/2001922405/1280/1280/0/180521-A-AE214-012.JPG" alt="" />
+                </div>
+                <div className="p-5">
+                    <small><span className='text-2xl'>As seen on:</span> Hell’s Kitchen, 24 Hours to Hell and Back, MasterChef
+                        <br />  <span className='text-2xl'> Cuisine speciality:</span> Traditional British dishes – especially his beef Wellington
+                        <br /> <span className='text-2xl'>Famous quote:</span> “I’ve had a lot of success; I’ve had failures, so I learn from the failures.”
+                       <br /> <span className='text-2xl'>Fun fact:</span> The only person to ever make Chef Ramsay cry in the kitchen is his daughter, <br /> Matilda. After her show aired on CBBC in 2014, she was devastated that her burger didn’t look as great as she wanted it to.</small>
                 </div>
             </div>
             <div className="lg:grid grid-rows-2 grid-flow-col gap-4">
@@ -45,9 +48,9 @@ const Home = () => {
                 }
             </div>
             <div className="">
-                <ThardBanner/>
+                <ThardBanner />
             </div>
-            
+
         </div>
     );
 };
